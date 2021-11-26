72198bb31d6eec4ac1e8a63f47d8da77ee816cc2
TheOnlyArtz <callcraft456@gmail.com>
2021-11-26 20:26:06 +0300

    Handling connection endpoint exchange

--- src/TCPServer.ts
+++ src/TCPServer.ts
@@ -1,23 +1,24 @@
 import ServerOptions from "@interfaces/ServerOptions";
 import { EventEmitter } from "events";
-import { Server, Socket} from "net";
+import { Server, Socket } from "net";
 import Node from '@interfaces/Node';
 import NodeState from "./enums/NodeState";
 import EventType from "./enums/EventType";
 import Message from "@interfaces/Message";
+import EndpointExchangeMessage from "@interfaces/EndpointExchangeMessage";
 
 export default class TCPServer extends EventEmitter {
     /**
      * Options to pass for the server itself
      */
     readonly options: ServerOptions;
-    
+
     /**
      * An array which tracks all of the available nodes
      * within the network
      */
     trackedConnections: Map<Socket, Node> = new Map;
-    
+
     /**
      * @param options The options to dictate the behavior of the server
      */
@@ -33,7 +34,7 @@ export default class TCPServer extends EventEmitter {
         const server = new Server();
 
         server.on("error", (e) => {
-            throw e;
+            console.error(e);
         });
 
         server.on("connection", this.handleConnection.bind(this));
@@ -63,16 +64,68 @@ export default class TCPServer extends EventEmitter {
         socket.on("data", (data: Buffer) => {
             try { JSON.parse(data.toString()) } catch { return }
 
+            // JSON needs to parsed since we get a buffer
             const parsed = JSON.parse(data.toString('utf8')) as Message;
-            
+
             switch (newNode.state) {
                 case NodeState.ENDPOINT_EXCHANGE:
-                    console.log(parsed);
+                    const data = parsed.d as EndpointExchangeMessage;
+
+                    this.handleEndpointExchange(data, newNode, socket);
+                    this.sendInitialHosts(socket);
+                    
+                    newNode.state = NodeState.OPERATIONAL;
+
                     break;
                 default:
                     console.log("RAW", parsed);
                     break;
             }
         });
+
+        /**
+         * Upon socket close we want to get rid of that socket if it's
+         * inside of trackedConnections
+         */
+        socket.on("close", () => {
+            this.trackedConnections.delete(socket);
+        });
+    }
+
+    /**
+     * Whenever a new node connects
+     * we want to inform them of the current list
+     * of nodes they should connect to
+     * @param socket A socket to send the hosts to
+     */
+    sendInitialHosts(socket: Socket) {
+        // We want to send all of the sockets
+        // while not including the current socket
+        const filteredKeys = Array
+            .from(this.trackedConnections.keys())
+            .filter((s: Socket) => s != socket);
+
+        const filteredSockets = filteredKeys
+            .map((s: Socket) => this.trackedConnections.get(s));
+
+        socket.write(JSON.stringify({
+            op: "INITIAL_SOCKETS",
+            d: filteredSockets
+        }));
+    }
+
+    /**
+     * @param data The endpoint exchange data
+     * @param newNode The node which is assigned to this data
+     * @param socket The socket which is assigned to this node
+     */
+    private handleEndpointExchange(data: EndpointExchangeMessage, newNode: Node, socket: Socket) {
+        if (!data.hasOwnProperty("localAddress") || !data.hasOwnProperty("localPort"))
+            throw "Missing localAddress or localPort"
+
+        newNode.localPort = data.localPort;
+        newNode.localAddress = data.localAddress;
+
+        this.trackedConnections.set(socket, newNode);
     }
 }
